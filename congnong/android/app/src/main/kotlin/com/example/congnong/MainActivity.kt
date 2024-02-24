19ebc9f8f56f2a00b610ef768785528df2bd1bf1
pnngnas <huusangvtvip@gmail.com>
2024-02-25 01:27:31 +0700

    up load flutter content

---
+++ congnong/android/app/src/main/kotlin/com/example/congnong/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.congnong
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ congnong/lib/appconstant.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+const greenBack = Color.fromARGB(255, 170, 227, 160);
+const buttonBack = Color.fromARGB(255, 4, 120, 87);
+final urlAPI = Uri.http("35.247.138.127", "api-ai/predict");
+
+final background = Color.fromARGB(255, 245, 245, 245);
+final apilua = 'http://35.247.138.127/api-ai/predict';
+final apitintuc = 'http://35.247.138.127/api-ai/tintucnongnghiep';
+final apiGoogleMapKey = 'AIzaSyCRJnsaJcohrs_D3q34AmtXjsgsQDpI1WQ';
